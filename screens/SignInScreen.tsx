9e2fd2f29159b6acb40cd6f79f4584566cca997f
Gin <hoangphuc442266@gmail.com>
2023-07-24 18:23:50 +0700

    update

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const SignInScreen = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={styles.container}>
       <Text>Sign in screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default SignInScreen;
